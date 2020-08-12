bab22119944b0457204e8d947fe048516fd2f576
abelgasque <55210807+abelgasque@users.noreply.github.com>
2020-08-12 20:46:45 -0300

    Crud usuario

--- Core.WebAPI/Controllers/UsuariosController.cs
+++ Core.WebAPI/Controllers/UsuariosController.cs
@@ -10,9 +10,9 @@ using Core.WebAPI.Models;
 namespace Core.WebAPI.Controllers
 {
     
-    [ApiController]
+    
     [Route("api/[controller]")]
-
+    [ApiController]
     public class UsuariosController : Controller
     {
         private readonly AppDataContext _context;
@@ -27,5 +27,70 @@ namespace Core.WebAPI.Controllers
         {
             return _context.Usuarios.ToList();
         }
+
+        // GET: api/TodoItems/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Usuario>> GetTodoItem(long id)
+        {
+            var todoItem = await _context.Usuarios.FindAsync(id);
+
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            return todoItem;
+        }
+
+        // POST: api/TodoItems
+        [HttpPost]
+        public async Task<ActionResult<Usuario>> PostTodoItem(Usuario todoItem)
+        {
+            _context.Usuarios.Add(todoItem);
+            await _context.SaveChangesAsync();
+
+            //return CreatedAtAction("GetTodoItem", new { id = todoItem.Id }, todoItem);
+            return CreatedAtAction(nameof(GetTodoItem), new { id = todoItem.Id }, todoItem);
+        }
+
+        // PUT: api/TodoItems/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTodoItem(long id, Usuario usuario)
+        {
+            if (id != usuario.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(usuario).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/TodoItems/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Usuario>> DeleteTodoItem(long id)
+        {
+            var entity = await _context.Usuarios.FindAsync(id);
+            if (entity == null)
+            {
+                return NotFound();
+            }
+
+            _context.Usuarios.Remove(entity);
+            await _context.SaveChangesAsync();
+
+            return entity;
+        }
+
     }
 }
--- Core.WebAPI/Models/Usuario.cs
+++ Core.WebAPI/Models/Usuario.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,22 @@ namespace Core.WebAPI.Models
 {
     public class Usuario
     {
+        [Key]
         public long Id { get; set; }
+
+        [Required(ErrorMessage = "Campo nome é obrigatorio!")]
+        [MaxLength(60, ErrorMessage = "Este campo deve conter no maximo 60 caracteres")]
+        [MinLength(3, ErrorMessage = "Este campo deve conter no minimo 3 caracteres")]
         public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Campo email é obrigatorio!")]
+        [MaxLength(60, ErrorMessage = "Este campo deve conter no maximo 60 caracteres")]
+        [MinLength(3, ErrorMessage = "Este campo deve conter no minimo 3 caracteres")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Campo senha é obrigatorio!")]
+        [MaxLength(60, ErrorMessage = "Este campo deve conter no maximo 60 caracteres")]
+        [MinLength(3, ErrorMessage = "Este campo deve conter no minimo 3 caracteres")]
         public string Senha { get; set; }
     }
 }
